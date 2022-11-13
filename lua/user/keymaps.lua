42a813bfab19005488162db3973564340f2b87f8
Kvoss <kvoss_git@freenet.de>
2022-11-13 16:25:13 +0100

    updated:    keymaps.lua changelog - added a comment for the mapping of the Lua_explorer

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -27,6 +27,7 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- mapping the explorer to <leader>e
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
