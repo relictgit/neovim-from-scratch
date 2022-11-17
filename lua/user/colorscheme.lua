197b354ee8bfdba82b2d66cba732f6b51d8dc32f
Kvoss <kvoss_git@freenet.de>
2022-11-17 19:17:54 +0100

    updated the colorscheme.lua file for checking if the main colorscheme is usabel. If not use the default color scheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,8 @@
-local colorscheme = "tokyonight-night"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+vim.cmd [[
+try
+  colorscheme tokyonight-night
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
