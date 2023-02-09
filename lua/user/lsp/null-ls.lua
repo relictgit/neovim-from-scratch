4abe92ab0d3acc7fe78a3b5c5c0c3deb1564ead5
Kvoss <kvoss_git@freenet.de>
2023-02-09 17:02:40 +0100

    commented out flake8

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,15 +8,15 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
-    formatting.stylua,
-    formatting.clang_format,
-    diagnostics.flake8,
-    diagnostics.clang_check,
-  },
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.yapf,
+		formatting.stylua,
+		formatting.clang_format,
+		-- diagnostics.flake8, -- flake8 is not installed on this system, so it will be deactivated
+		diagnostics.clang_check,
+	},
+})
