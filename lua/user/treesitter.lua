3830b8898e65b0b1c85254d7512c6c228b72604c
relictgit <95646932+relictgit@users.noreply.github.com>
2023-09-07 02:47:51 +0200

    Update treesitter.lua
    set sync_install to true

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false,
+  sync_install = true,
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
